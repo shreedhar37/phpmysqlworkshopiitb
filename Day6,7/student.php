8b1de776e091209ac7391cf3c4736fddfe5450cf
shreedhar37 <meshree4@gmail,com>
2020-10-13 14:42:57 +0530

    First Commit. New code files added

---
+++ Day6,7/student.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+
+    print('<div style="text-align:center;">
+             <br> <br>
+            <form action = "slogin.php" method="POST">
+            
+            <input type="submit" value="Login" name="login"><br><br>
+            </form>
+            
+            <form action = "sregister.php" method= "POST">
+            <input type="submit" value= "Register" name="register"><br><br>
+            </form>
+            
+            </div>');
+ 
+        
+ 
+ ?>
